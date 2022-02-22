cdd2d017bc58eb91ad3d3d348f61587b77941e28
DavidGGL <94107134+DavidGGL@users.noreply.github.com>
2022-02-22 17:16:50 +0100

    Saludo
    Saludo personalizado

--- Hola_mundo.cpp
+++ Hola_mundo.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+	string nombre;
 	cout<<"Hola mundo\n";
+	cout<<"¿Cómo te llamas?\n";
+	cin>>nombre;
+	cout<<"Encantado " << nombre;
 	return 0;
 }
 
